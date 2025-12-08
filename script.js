d5856af6752d6d1a8714ef42955bfc611501ffa9
Serd0r <serdarkimaq@gmail.com>
2025-12-08 19:43:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+const menuToggle = document.querySelector('.menu-toggle');
+const navbar = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
